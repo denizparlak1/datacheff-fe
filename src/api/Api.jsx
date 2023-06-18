e34ac78261a8bb0a11e5ec548e662de67394f53a
denizparlak1 <60672820+denizparlak1@users.noreply.github.com>
2023-06-18 12:48:59 +0300

    Update Api.jsx
    Update prod api

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8000"
-
+//const BASE_URL = "http://localhost:8000"
+const BASE_URL = "ec2-52-51-114-67.eu-west-1.compute.amazonaws.com"
 export const API = {
     getCampaigns: BASE_URL+'/campaigns/',
     uploadCSVData: BASE_URL+'/upload/csv/api/'
