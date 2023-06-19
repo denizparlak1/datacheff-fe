5a083da51a393511df8b80bc137cc8549d5c3f69
denizparlak1 <60672820+denizparlak1@users.noreply.github.com>
2023-06-19 15:06:36 +0300

    Update Api.jsx

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://ip-10-0-9-248:8000"
+const BASE_URL = "http://ec2-52-51-114-67.eu-west-1.compute.amazonaws.com:8000"
 //const BASE_URL = "http://ec2-52-51-114-67.eu-west-1.compute.amazonaws.com:8000"
 export const API = {
     getCampaigns: BASE_URL+'/campaigns/',
